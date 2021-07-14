8168ece18baab5bce381f06972317392a4be08b6
Ghislain d'Andlau <ghislain.dandlau@gmail.com>
2021-07-14 20:49:56 +0200

    exc 2, 13 July

---
+++ app/models/restaurant.rb
@@ -0,0 +1,7 @@
+class Restaurant < ApplicationRecord
+  has_many :reviews, dependent: :delete_all
+  validates :name, :address, :category, presence: true
+  validates :category, inclusion: { in: %w(chinese italian japanese french belgian),
+    message: "%{value} is not a valid category" }
+
+end
